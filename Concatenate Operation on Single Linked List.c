4ea49ed7a3d2197a501564e63d3b367335b960da
roshanseerwani83-debug <roshanseerwani83@gmail.com>
2026-02-11 14:12:41 +0530

    Implement concatenate operation for linked lists

---
+++ Concatenate Operation on Single Linked List.c
@@ -0,0 +1,65 @@
+struct node {
+    int data;
+    struct node* next;
+};
+
+typedef struct node* NODE;
+
+NODE createNode(int x) {
+      NODE temp = (NODE)malloc(sizeof(struct node));
+    temp->data = x;
+    temp->next = NULL;
+    return temp;  
+    
+    
+    
+}
+
+NODE addNode(NODE first, int x) {
+    NODE temp = createNode(x);
+
+    if (first == NULL) {
+        return temp;
+    }
+
+    NODE curr = first;
+    while (curr->next != NULL) {
+        curr = curr->next;
+    }
+    curr->next = temp;
+    return first;
+    
+    
+}
+
+NODE concatenate(NODE t1, NODE t2) {
+    if (t1 == NULL)
+        return t2;
+    if (t2 == NULL)
+        return t1;
+
+    NODE temp = t1;
+    while (temp->next != NULL) {
+        temp = temp->next;
+    }
+    temp->next = t2;
+    return t1;
+    
+    
+    
+    
+    
+}
+
+void displayList(NODE first) {
+    NODE temp = first;
+    while (temp != NULL) {
+        printf("%d --> ", temp->data);
+        temp = temp->next;
+    }
+    printf("NULL\n");
+    
+    
+    
+    
+}
